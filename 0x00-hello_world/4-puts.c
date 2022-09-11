214400b96bd1872ae12c55c5e51998fa1facd44f
Ndully <nwtnjml@proton.me>
2022-09-10 22:10:01 -0700

    my first C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * My first C program that prints
+ *programming is like bulding multilingual puzzle
+ */
+char main (void)
+{	str="Programming is like buillding a multlingual puzzle
+	puts(str);
+
+Return:(0)
+
+}
