f51f24fca73c9ed23f17ebceb90a4432c1c26416
Ndully <nwtnjml@proton.me>
2022-09-10 23:46:37 -0700

    my first C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *programming is like bulding multilingual puzzle
  *return: 0 if success
  */
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");	
+intmain(void)
+{
+puts("\"Programming is like building a multilingual puzzle");	
 return (0);
-
 }
