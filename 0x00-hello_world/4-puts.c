8b51bbbec6ccfcb18428784fa896ba8c93c8446f
Ndully <nwtnjml@proton.me>
2022-09-10 23:41:04 -0700

    my first c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 /**
  * main prints My first C program that prints
  *programming is like bulding multilingual puzzle
- *Return: 0 if success
+ *return: 0 if success
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
+	puts("\"Programming is like building a multilingual puzzle");	
 return (0);
 
 }
