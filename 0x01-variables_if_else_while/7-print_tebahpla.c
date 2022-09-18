a34c6d065f635a54be9cba7a28918b74b723a01a
Ndully <nwtnjml@proton.me>
2022-09-18 04:19:49 -0700

    variable if else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdlib>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{int low;
+for (low = 'z'; low >= z; low--);
+	putchar(low);
+	putchar('\n');
+return (0);
+}
