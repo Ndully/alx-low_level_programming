2e37054aedfc2c8603d7d003d6b9d110c25b678d
Ndully <nwtnjml@proton.me>
2022-09-18 00:00:51 -0700

    sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main() - prints size of many variable types
+ *
+ * return = 0 if successful
+ */
+int main()
+{char a;
+int b;
+long int c;
+long long int d;
+float c;
+printf("Size of a char: %d byte(s)\n", sizeof(a));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
+printf("Size of a long int: %d byte(s)\n", sizeof(c));
+printf("Size of a long long int: %d byte(s)", sizeof(d));
+printf("Size of a float: %d byte(S)", sizeof(e));
+return (0);
+}
