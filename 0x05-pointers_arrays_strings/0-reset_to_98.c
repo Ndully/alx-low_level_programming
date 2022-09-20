9b5a38fe861d59e07b05175db636457b7bb28a8d
Ndully <nwtnjml@proton.me>
2022-09-19 19:58:49 -0700

    pointers array string

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - swaps value of two integers
+ *
+ * @a: int 1
+ * @b: int 2
+ * Return: void
+ */
+void swap_int(int *a, int *b);
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
