90685feb5ec8e488bf309d1890ae3a26d877091e
Ndully <nwtnjml@proton.me>
2022-09-22 14:55:08 -0700

    concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+
+#include <stdio.h>
+
+
+/**
+ * main - concatenate the srings
+ *
+ ** Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	char s1[4] = src;
+	char s2[5] = dest;
+	char *ptr;
+
+printf("%s\n", s1);
+printf("%s", s2);
+ptr = _strcat(s1, s2);
+printf("%s", s1);
+printf("%s", s2);
+printf("%s", ptr);
+
+return (0);
+}
