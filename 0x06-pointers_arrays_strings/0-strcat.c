43ca0220b9438ba70e0a82be08fce85f06015e92
Ndully <nwtnjml@proton.me>
2022-09-22 17:24:06 -0700

    more pointers arrays and string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,29 @@
-
 #include "main.h"
 
-#include <stdio.h>
-
-
 /**
- * main - concatenate the srings
- *@dest: copy to
- *@src: copy from
+ * _strcat - concatenate two strings
+ * @dest: copy to
+ * @src: copy from
  *
- * Return: Always 0
-
+ * Return: void
+ */
 char *_strcat(char *dest, char *src)
 {
-	char s1[4] = src;
-	char s2[5] = dest;
-	char *ptr;
+	int i;
+	int j;
 
-printf("%s\n", s1);
-printf("%s", s2);
-ptr = _strcat(s1, s2);
-printf("%s", s1);
-printf("%s", s2);
-printf("%s", ptr);
-
-return (0);
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+dest[i] = '\0';
+return (dest);
 }
