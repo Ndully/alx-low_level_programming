c8bc0073c7ac3ac59505bef55fedbdbf4b8d6d70
Ndully <nwtnjml@proton.me>
2022-09-22 20:24:50 -0700

    sizes of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,15 +12,11 @@ int main(void)
 	long long int d;
 	float e;
 
-a = 1;
-b = 4;
-c = 8;
-d = 8;
-e = 4;
-printf("Size of a char: %d byte(s)\n", sizeof(a));
-printf("Size of an int: %d byte(s)\n", sizeof(b));
-printf("Size of a long int: %d byte(s)\n", sizeof(c));
-printf("Size of a long long int: %d byte(s)", sizeof(d));
-printf("Size of a float: %d byte(S)", sizeof(e));
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)", sizeof(d));
+	printf("Size of a float: %d byte(S)", sizeof(e));
+
+	return (0);
 }
