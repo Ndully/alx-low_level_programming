a50965fb17aab4526abf2bfa2c9d9fe4bf24228c
Ndully <nwtnjml@proton.me>
2022-09-22 20:03:42 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,9 @@
  * @printf: prints out on screen
  *Return: Always 0
  */
-int main()
+int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,");
+	printf (" with proper grammar, but the outcome is a piece of art,");
 
 	return (0);
 }
